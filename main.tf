54b0ed59eb60898425448fcc0ca8ac2472567a5b
yuntyu01 <yuntyu01@gmail.com>
2025-10-03 21:33:01 +0900

    add alb

--- main.tf
+++ main.tf
@@ -28,6 +28,9 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+#################
+### routtable ###
+#################
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.prod.id
 
@@ -50,6 +53,11 @@ resource "aws_route_table_association" "pub_c" {
   subnet_id      = aws_subnet.prod_pub_c.id
   route_table_id = aws_route_table.public.id
 }
+
+##############
+### subnet ###
+##############
+
 resource "aws_subnet" "prod_pub_a" {
   vpc_id            = aws_vpc.prod.id
   cidr_block        = "10.0.10.0/24"
@@ -110,10 +118,13 @@ resource "aws_subnet" "prod_db_c" {
   }
 }
 
-
+################
+### instance ###
+################
 resource "aws_instance" "prod_was_a" {
   ami           = "ami-077ad873396d76f6a"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.prod_was_sg.id]
 
   subnet_id = aws_subnet.prod_was_a.id
   key_name  = "prod_key"
@@ -125,11 +136,145 @@ resource "aws_instance" "prod_was_a" {
 resource "aws_instance" "prod_was_c" {
   ami           = "ami-077ad873396d76f6a"
   instance_type = "t3.micro"
+  security_groups = [aws_security_group.prod_was_sg.id]
 
   subnet_id = aws_subnet.prod_was_c.id
   key_name  = "prod_key"
 
   tags = {
     Name = "prod_was_c"
+  }
+}
+
+resource "aws_security_group" "prod_was_sg" {
+  name        = "prod-was-sg"
+  description = "Security group for WAS servers"
+  vpc_id      = aws_vpc.prod.id
+
+  # 인바운드 규칙: ALB에서 오는 HTTP 허용
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow HTTP from anywhere"
+  }
+
+    ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow SSH from anywhere"
+  }
+  
+  
+
+  # 아웃바운드 규칙: 모든 트래픽 허용
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "prod-was-sg"
+  }
+}
+
+########################
+### ALB, TargetGroup ### 
+########################
+
+resource "aws_lb" "prod_lb" {
+  name               = "prod-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.prod_alb_sg.id]
+  subnets            = [aws_subnet.prod_pub_a.id, aws_subnet.prod_pub_c.id]
+
+
+  tags = {
+    Environment = "prod"
+  }
+}
+
+
+resource "aws_lb_listener" "prod_listener" {
+  load_balancer_arn = aws_lb.prod_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prod_was_tg.arn
+  }
+}
+
+resource "aws_lb_target_group" "prod_was_tg" {
+  name     = "prod-was-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.prod.id
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "prod_was_tg"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "prod_was_a_attach" {
+  target_group_arn = aws_lb_target_group.prod_was_tg.arn
+  target_id        = aws_instance.prod_was_a.id
+  port             = 80
+}
+resource "aws_lb_target_group_attachment" "prod_was_c_attach" {
+  target_group_arn = aws_lb_target_group.prod_was_tg.arn
+  target_id        = aws_instance.prod_was_c.id
+  port             = 80
+}
+
+resource "aws_security_group" "prod_alb_sg" {
+  name        = "prod-alb-sg"
+  description = "Security group for public ALB"
+  vpc_id      = aws_vpc.prod.id
+
+  # 인바운드 규칙: 외부 HTTP/HTTPS 허용
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP from anywhere"
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTPS from anywhere"
+  }
+
+  # 아웃바운드 규칙: 모든 트래픽 허용
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "prod-alb-sg"
   }
 }
